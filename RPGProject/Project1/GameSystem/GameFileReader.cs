b693ac7a89366f66f6e8a71716558e4b73b96528
cafeunder <cafe3641@gmail.com>
2014-11-14 19:22:43 +0900

    ファイルの追加しなおし
    2014/11/14 19:22
    投稿者 : モリシタ
    
    gitignoreが適用されたことを確認してファイルを追加しなおした。

---
+++ RPGProject/Project1/GameSystem/GameFileReader.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DxLibDLL;
+
+namespace RPGProject.GameSystem {
+	/// <summary>
+	/// ファイル読み込み機能を提供するクラス
+	/// このクラスはアーカイバにまとめられたファイルを読み込むことができる
+	/// </summary>
+	class GameFileReader {
+		/// <summary>
+		/// ファイルパス
+		/// </summary>
+		readonly public string FilePath;
+		private int fileHandle;	//ファイルハンドル
+
+
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="filePath">読み込むファイル名</param>
+		public GameFileReader(string filePath){
+			FilePath = filePath;
+			fileHandle = DX.FileRead_open(filePath);
+
+			if(fileHandle == 0){
+				Program.AssertExit("不正なファイル" + filePath + "が指定されました。");
+			}
+		}
+
+		/// <summary>
+		/// デストラクタ（内部でファイルクローズが呼び出されている）
+		/// </summary>
+		~GameFileReader(){
+			if(fileHandle != -1){
+				DX.FileRead_close(fileHandle);
+			}
+		}
+
+		/// <summary>
+		/// ファイルからbyteだけ読み込むメソッド
+		/// </summary>
+		/// <returns>読み込んだbyteデータ</returns>
+		public byte ReadByte(){
+			if(fileHandle == -1){
+				FileisAlreadyClosed();
+			}
+
+			byte result = 0;
+			unsafe{
+				DX.FileRead_read(&result, sizeof(byte), fileHandle);
+			}
+			return result;
+		}
+		
+		/// <summary>
+		/// ファイルからintだけ読み込むメソッド
+		/// </summary>
+		/// <returns>読み込んだintデータ</returns>
+		public int ReadInt(){
+			if(fileHandle == -1){
+				FileisAlreadyClosed();
+			}
+
+			int result = 0;
+			unsafe{
+				DX.FileRead_read(&result, sizeof(int), fileHandle);
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// ファイルからdoubleだけ読み込むメソッド
+		/// </summary>
+		/// <returns>読み込んだdoubleデータ</returns>
+		public double ReadDouble(){
+			if(fileHandle == -1){
+				FileisAlreadyClosed();
+			}
+
+			double result = 0;
+			unsafe{
+				DX.FileRead_read(&result, sizeof(double), fileHandle);
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// ファイルから一行読み込むメソッド
+		/// </summary>
+		/// <param name="lineSize">読み込む文字列の最大長</param>
+		/// <returns>読み込んだ文字列</returns>
+		public string ReadLine(int lineSize){
+			if(fileHandle == -1){
+				FileisAlreadyClosed();
+			}
+
+			StringBuilder input = new StringBuilder(0);
+			DX.FileRead_gets(input, lineSize, fileHandle);
+			return input.ToString();
+		}
+
+		/// <summary>
+		/// ファイルを自発的に閉じるメソッド
+		/// </summary>
+		public void Close(){
+			if(fileHandle == -1){
+				FileisAlreadyClosed();
+			}
+
+			DX.FileRead_close(fileHandle);
+			fileHandle = -1;
+		}
+
+		private void FileisAlreadyClosed(){
+			Program.AssertExit("閉じられたファイル" + FilePath + "の内容を読み込もうとしました。");
+		}
+	}
+}
---
+++ RPGProject/Project1/GameSystem/GameFont.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGProject.GameSystem {
+	//フォントデータを管理するクラス
+
+	//フォントの登録方法 :
+	// フォントデータ管理配列に、フォントデータを追加する
+	// 構文 : new FontData("登録名", "フォントネーム", 文字の大きさ, 文字の太さ[-1でデフォルト], アンチエイリアスをするかどうか),
+	//
+	// デフォルトの文字の太さは6ptです。
+	//
+	// 例　 : MS ゴシック 16pt をデフォルトの太さで、アンチエイリアスなしで登録
+	//		  名前は"SystemFont"
+	// new FontData("SystemFont","MS ゴシック",16,6,false),
+	static class GameFont{
+
+		//======================
+		//| 画像データ管理配列 |
+		//======================
+		private static FontData[] fontData = new FontData[]{
+			//ここに追加していく
+			new FontData("system","MS ゴシック",16,6,false),
+		};
+
+
+
+		//*************************
+		//*　以下は変更しないこと *
+		//*************************
+
+		private struct FontData{
+			public string name;
+			public string fontName;
+			public int size;
+			public int thick;
+			public bool antiAlias;
+
+			public FontData(string name, string fontName, int size, int thick, bool antiAlias){
+				this.name = name;
+				this.fontName = fontName;
+				this.size = size;
+				this.thick = thick;
+				this.antiAlias = antiAlias;
+			}
+		}
+		private static Dictionary<string, int> fontTable = new Dictionary<string,int>();
+		public static void LoadFont(){
+			foreach(var f in fontData){
+				int type = DxLibDLL.DX.DX_FONTTYPE_ANTIALIASING;
+				if(!f.antiAlias) type = DxLibDLL.DX.DX_FOGMODE_NONE;
+
+				fontTable.Add(f.name, DxLibDLL.DX.CreateFontToHandle(f.fontName, f.size, f.thick, type));
+			}
+		}
+		public static int GetFont(string name){
+			int result;
+			bool exist = fontTable.TryGetValue(name, out result);
+
+			if(!exist){
+				Program.AssertExit("登録されていないフォント" + name + "が指定されました。");
+			}
+
+			return result;
+		}
+	}
+}
