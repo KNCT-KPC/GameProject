e091047e7bfc5a4617c99c89d07367d820ca87f0
stelthpon <ponsan.dq.special.player0501@gmail.com>
2014-11-14 23:02:23 +0900

    Player,PlayerStatusの追加
    2014/11/14 23:01
    投稿者:谷口
    
    otherテキストの内容

---
+++ RPGProject/Project1/GamePlay/Database/PlayerStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGProject.GamePlay.Party
+{
+    class PlayerStatus
+    {
+        public readonly int HP;
+        public readonly int TP;
+        public readonly int 筋力;
+        public readonly int 耐久;
+        public readonly int 精神;
+        public readonly int 技能;
+        public readonly int 俊敏;
+        public readonly int 幸運;
+    }
+}
