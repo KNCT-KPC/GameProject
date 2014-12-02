61c7c0b7f041f3c9c72fa340d3e288aaafa412ec
cafeunder <cafe3641@gmail.ocm>
2014-12-02 18:01:50 +0900

    高橋先生が帰るみたいなのでpush
    2014/12/02 18:01
    投稿者 : モリシタ
    
    無言の圧力があるのでドキュメントを書いている時間が無い

---
+++ RPGProject/Project1/GamePlay/FrontEnd/TimeMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGProject.GamePlay.FrontEnd {
+	class TimeMessage{
+		const int DEBUG_MAX_COUNT = 30;
+		const int DEBUG_LINE_CHAR = 10;
+		const int DEBUG_LINE_NUM = 3;
+
+		private int count = 0;
+		private int line = 0;
+		private int charIndex = 0;
+		private bool finish = false;
+		private string message;
+		private string[] subMessage;
+
+		public TimeMessage(string message){
+			this.message = message;
+			subMessage = new string[DEBUG_LINE_NUM];
+
+			subMessage[line] = "";
+		}
+
+		public void NextCount(){
+			if(charIndex >= message.Length){
+				finish = true;
+				return;
+			}
+
+			count++;
+
+			if(count >= DEBUG_MAX_COUNT){
+				count = 0;
+				subMessage[line] += message[charIndex];
+				charIndex++;
+			}
+		}
+
+		public string[] GetMessage(){
+			return subMessage;
+		}
+
+		public bool isFinished(){
+			return finish;
+		}
+	}
+}
