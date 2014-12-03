813d9f3c9d2c0099db188d61854af4becfbe8c42
Nezzumiwpw <p120329@cc.kushiro-ct.ac.jp>
2014-12-03 23:28:09 +0900

    マップの確認の為のコミット
    2014/12/3 23:27
    投稿者：根塚康介
    
    いまいち動作の確認の仕方がわからないのでファイルを上げて相部屋に確認してもらうために更新しました
    まだ動作確認済んでません

---
+++ RPGProject/Project1/GamePlay/Map/Map.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RPGProject.GameSystem;
+using DxLibDLL;
+
+namespace RPGProject.GamePlay.Map
+{
+    class Map
+    {
+        int xSize;
+        int ySize;
+        int px;
+        int py;
+        int[,] drawTip;
+        int[,] stateTip;
+        public void Map(int arg_xSize, int arg_ySize)
+        {
+            xSize = arg_xSize;
+            ySize = arg_ySize;
+            drawTip = new int[xSize, ySize];
+        }
+        public void Update()
+        {
+            int mx;
+            int my;
+            if (Input.GetCount(Input.Code.Up) > 0)
+            {
+                my = py - 1;
+            }
+            if (Input.GetCount(Input.Code.Down) > 0)
+            {
+                my = py + 1;
+            }
+            if (Input.GetCount(Input.Code.Left) > 0)
+            {
+                mx = px - 1;
+            }
+            if (Input.GetCount(Input.Code.Right) > 0)
+            {
+                mx = px + 1;
+            }
+            if ((mx < 0) || (my < 0) || (mx >= xSize) || (my >= ySize))
+            {
+                my = py;
+                mx = px;
+            }
+            if (stateTip[my, mx] == 1)
+            {
+                my = py;	//戻す
+                mx = px;	//戻す
+            }
+            px = mx;
+            py = my;
+        }
+        public void Draw()
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                for (int x = 0; x < xSize; x++)
+                {
+                    if (drawTip[y, x] == 0)
+                    {
+                        DxLibDLL.DX.DrawBox(x, y, x + 32, y + 32, DX.GetColor(255, 255, 255), 1);
+                    }
+                    else if (drawTip[y, x] == 0)
+                    {
+                        DxLibDLL.DX.DrawBox(x, y, x + 32, y + 32, DX.GetColor(0, 0, 0), 1);
+                    }
+                }
+            }
+            DxLibDLL.DX.DrawBox(px, py, px + 32, py + 32, DX.GetColor(0, 255, 0), 1);
+        }
+    }
+}
