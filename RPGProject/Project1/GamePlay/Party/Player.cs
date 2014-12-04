f91653f5b93b8c8ed59bb9e6c91d0038ed393beb
stelthpon <ponsan.dq.special.player0501@gmail.com>
2014-12-04 22:03:54 +0900

    Player,PlayerStatusの変更
    2014/12/4 22:02
    投稿者:たにぐち
    
    Player,PlayerStatusの内容を変更しました。

--- RPGProject/Project1/GamePlay/Party/Player.cs
+++ RPGProject/Project1/GamePlay/Party/Player.cs
@@ -7,5 +7,53 @@ namespace RPGProject.GamePlay.Party
 {
     class Player
     {
+        string name;
+        PlayerStatus status;
+        int Lv;
+        int HP;
+        int TP;
+
+        public void NameStatus(string Name, PlayerStatus Status, int hp, int tp) {
+            this.name = Name;
+            this.status = Status;
+            this.HP = hp;
+            this.TP = tp;
+        }
+
+        public int GetHP() {
+            return HP;
+        }
+
+        public int GetTP() {
+            return TP;
+        }
+
+        public void SetHP(int MAXHP) {
+            if (HP < 0) {
+                HP = 0;
+            }
+
+            if (HP >= MAXHP) {
+                HP = MAXHP;
+            }
+        }
+
+        public void SetTP(int MAXTP) {
+            if (TP < 0) {
+                TP = 0;
+            }
+
+            if (TP >= MAXTP){
+                TP = MAXTP;
+            }
+        }
+
+        public PlayerStatus GetStatus() {
+            return status;
+        }
+
+        public int GetLv() {
+            return Lv;
+        }
     }
 }
--- RPGProject/Project1/GamePlay/Party/PlayerStatus.cs
+++ RPGProject/Project1/GamePlay/Party/PlayerStatus.cs
@@ -7,8 +7,8 @@ namespace RPGProject.GamePlay.Party
 {
     class PlayerStatus
     {
-        public readonly int HP;
-        public readonly int TP;
+        public readonly int MaxHP;
+        public readonly int MaxTP;
         public readonly int 筋力;
         public readonly int 耐久;
         public readonly int 精神;
@@ -16,10 +16,10 @@ namespace RPGProject.GamePlay.Party
         public readonly int 俊敏;
         public readonly int 幸運;
 
-        public PlayerStatus(int hp, int tp, int 筋力, int 耐久, int 精神, int 技能, int 俊敏, int 幸運)
+        public PlayerStatus(int maxhp, int maxtp, int 筋力, int 耐久, int 精神, int 技能, int 俊敏, int 幸運)
         {
-            this.HP = hp;
-            this.TP = tp;
+            this.MaxHP = maxhp;
+            this.MaxTP = maxtp;
             this.筋力 = 筋力;
             this.耐久 = 耐久;
             this.精神 = 精神;
