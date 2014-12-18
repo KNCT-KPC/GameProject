f31a788c3e722a2ab16beb52bd07d604c3627ea3
stelthpon <ponsan.dq.special.player0501@gmail.com>
2014-12-18 22:09:44 +0900

    FlagMgrの追加
    2014/12/18 22:09
    投稿者:たにぐち
    
    FlagMgr.cs

---
+++ RPGProject/Project1/GamePlay/Party/FlagMgr.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGProject.GamePlay.Control
+{
+    class FlagMgr
+    {
+        Dictionary<string, bool> flags;
+
+        /// <summary>
+        /// flagsの初期化
+        /// </summary>
+        /// <param name="names">文字列配列</param>
+        public FlagMgr(string[] names) {
+            for (int i = 0; i < names.Length; i++) {
+                flags[names[i]] = false;
+            }
+        }
+
+        /// <summary>
+        /// フラグをセットする
+        /// </summary>
+        /// <param name="names">フラグ名</param>
+        /// <param name="flag">bool値</param>
+        public void SetFlag(string names, bool flag) {
+            flags[names] = flag;
+        }
+
+        /// <summary>
+        /// フラグを反転する
+        /// </summary>
+        /// <param name="names">フラグ名</param>
+        public void ReverseFlag(string names) {
+            flags[names] = true;
+
+            if (flags[names]) {
+                flags[names] = false;
+            } else {
+                flags[names] = true;
+            }
+        }
+
+        /// <summary>
+        /// フラグを返す
+        /// </summary>
+        /// <param name="names">フラグ名</param>
+        /// <returns></returns>
+        public bool GetFlag(string names) {
+            return (flags[names]);
+        }
+    }
+}
