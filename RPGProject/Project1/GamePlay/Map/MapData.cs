75336cf878cb2f8aaeeecdee6a0ff77f465ef7bd
stelthpon <ponsan.dq.special.player0501@gmail.com>
2015-04-21 22:49:16 +0900

    MapData.cs 追加
    2015/04/21 22:49
    投稿者:たにぐち

---
+++ RPGProject/Project1/GamePlay/Map/MapData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGProject.GamePlay.Map
+{
+    class MapData
+    {
+        /// <summary>
+        /// マップデータのメンバ
+        /// </summary>
+        readonly public string mapName;
+        readonly public int xSize;
+        readonly public int ySize;
+        readonly public int[,] stateTip;
+        readonly public int[,] drawTip;
+        
+        /// <summary>
+        /// マップデータの初期化
+        /// </summary>
+        /// <param name="mapname"></param>
+        /// <param name="xsize"></param>
+        /// <param name="ysize"></param>
+        /// <param name="statetip"></param>
+        /// <param name="drawtip"></param>
+        public MapData(string mapname, int xsize, int ysize, int[,] statetip, int[,] drawtip) {
+            this.mapName = mapname;
+            this.xSize = xsize;
+            this.ySize = ysize;
+            this.stateTip = statetip;
+            this.drawTip = drawtip;
+        }
+    }
+}
--- RPGProject/Project1/GamePlay/Party/PlayerUnitStatus.cs
+++ RPGProject/Project1/GamePlay/Party/PlayerUnitStatus.cs
@@ -27,8 +27,7 @@ namespace RPGProject.GamePlay.Party
         /// <param name="技能">技能</param>
         /// <param name="俊敏">俊敏</param>
         /// <param name="幸運">幸運</param>
-        public PlayerUnitStatus(int maxhp, int maxtp, int 筋力, int 耐久, int 精神, int 技能, int 俊敏, int 幸運)
-        {
+        public PlayerUnitStatus(int maxhp, int maxtp, int 筋力, int 耐久, int 精神, int 技能, int 俊敏, int 幸運) {
             this.MaxHP = maxhp;
             this.MaxTP = maxtp;
             this.筋力 = 筋力;
